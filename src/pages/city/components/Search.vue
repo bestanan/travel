f6d80e333ab97820bc9149da10c0501cf61626de
bestanan <864814910@qq.com>
2020-11-16 12:18:00 +0800

    完成搜索逻辑

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,63 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input class="search-input" type="text" placeholder="输入城市名或拼音" v-model="keyword">
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">
+          {{item.name}}
+        </li>
+        <li class="search-item border-bottom nodata" v-show="noData">
+          未找到数据奥~
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
+
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: []
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  },
+  updated () {
+    this.scroll.refresh()
+  },
+  computed: {
+    noData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      let result = []
+      for (let key in this.cities) {
+        this.cities[key].forEach(element => {
+          if (element.name.indexOf(this.keyword) > -1 || element.spell.indexOf(this.keyword) > -1) {
+            result.push(element)
+          }
+        })
+      }
+      this.list = result
+    }
+  }
 }
 </script>
 
@@ -25,4 +76,21 @@ export default {
       text-align center
       border-radius .06rem
       color #666
+  .search-content
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    background #eee
+    z-index 1
+    overflow hidden
+    .search-item
+      height .62rem
+      line-height .62rem
+      padding-left .2rem
+      color #666
+      background #fff
+    .nodata
+      text-align center
 </style>
